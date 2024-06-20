7aaed5fdc4211b24e177ec8aff0c1bee89d19755
saurabhkr66 <sauravkum420@gmail.com>
2024-06-20 15:26:27 +0530

    set up backend

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from "express"
+import dotenv from "dotenv"
+import authRoutes from "./routes/auth.routes.js"
+const app=express();
+
+dotenv.config();
+const PORT=process.env.PORT || 5000;
+app.get("/",(req,res)=>{
+    res.send("hello  3world")
+})
+
+app.use("/api/auth",authRoutes)
+app.listen(PORT,()=>console.log(`server running on port ${PORT}`));
