68f2184ab500855bb3ef697ab84ab8113dad9f71
saurabhkr66 <sauravkum420@gmail.com>
2024-07-12 06:45:37 +0530

    frontend

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -5,6 +5,12 @@ const Messages = () => {
   return (
     <div className='px-4 flex-1 overflow-auto'>
       <Msg/>
+      <Msg/>
+      <Msg/>
+      <Msg/>
+      <Msg/>
+      <Msg/>
+      <Msg/><Msg/><Msg/><Msg/><Msg/><Msg/>
     </div>
   )
 }
